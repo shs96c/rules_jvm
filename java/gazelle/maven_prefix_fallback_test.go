a4b1077bb0046e8e303113db144947c3a4736ef6
Simon Mavi Stewart <shs@squareup.com>
2026-07-24 17:12:12 +0100

    0008-outer-class-maven-resolution-fallbacks
    Co-authored-by: Simon Mavi Stewart <shs@squareup.com>
    Signed-off-by: Simon Mavi Stewart <shs@squareup.com>

---
+++ java/gazelle/maven_prefix_fallback_test.go
@@ -0,0 +1,57 @@
+package gazelle
+
+import (
+	"testing"
+
+	"github.com/bazel-contrib/rules_jvm/java/gazelle/javaconfig"
+	"github.com/bazel-contrib/rules_jvm/java/gazelle/private/maven"
+	"github.com/bazel-contrib/rules_jvm/java/gazelle/private/types"
+	"github.com/bazelbuild/bazel-gazelle/label"
+)
+
+type prefixMavenResolver map[string]label.Label
+
+func (r prefixMavenResolver) Resolve(pkg types.PackageName, excludedArtifacts map[string]struct{}, mavenRepositoryName string) (label.Label, error) {
+	if l, ok := r[pkg.Name]; ok {
+		return l, nil
+	}
+	return label.NoLabel, &maven.NoExternalImportsError{PackageName: pkg.Name}
+}
+
+func (r prefixMavenResolver) ResolveClass(className types.ClassName, excludedArtifacts map[string]struct{}, mavenRepositoryName string) (label.Label, error) {
+	return label.NoLabel, nil
+}
+
+func TestResolveMavenWholePackageClassLongestPrefix(t *testing.T) {
+	className, err := types.ParseClassName("org.xbill.DNS.Lookup")
+	if err != nil {
+		t.Fatal(err)
+	}
+	want := label.New("maven", "", "dnsjava")
+	lang := &javaLang{mavenResolver: prefixMavenResolver{
+		"org":           label.New("maven", "", "too_short"),
+		"org.xbill.DNS": want,
+	}}
+	resolver := NewResolver(lang)
+	config := javaconfig.New(".")
+	got, err := resolver.resolveMavenWholePackageClass(config, *className)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if got != want {
+		t.Fatalf("resolveMavenWholePackageClass() = %s, want %s", got, want)
+	}
+}
+
+func TestResolveMavenWholePackageClassNoUniquePrefix(t *testing.T) {
+	className, err := types.ParseClassName("org.apache.spark.sql.functions.col")
+	if err != nil {
+		t.Fatal(err)
+	}
+	lang := &javaLang{mavenResolver: prefixMavenResolver{}}
+	resolver := NewResolver(lang)
+	got, err := resolver.resolveMavenWholePackageClass(javaconfig.New("."), *className)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if got != label.NoLabel {
